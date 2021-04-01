899a71502b37525c014de38a8c9b667cb5501c5a
Ivan-Grudev <vanek-stark@yandex.ru>
2021-04-01 15:56:55 +0300

    поменял функцию поиска, убрал лишние библиотеки, добавил ввод подстроки пользователем

--- Задача 2/main.cpp
+++ Задача 2/main.cpp
@@ -10,139 +10,68 @@
 #include <iterator>
 #include <numeric>
 #include <string>
+#include <functional>
+
 using namespace std;
 
-void Rand_Init (int n)
+int If_eq(char a, char b)
 {
+    if (a == b)
+    {
+        return 1;
+    }
+    else return 0;
 }
 
-//int Rand(int n)
-//{
-//    return dist(gen);
-//}
+std::mutex _mutex;
 
-
-class Threads_Guard
+void Search(std::string main_string, std::string substring, std::vector<size_t>& Iterators, size_t left, size_t right)
 {
-public:
+    _mutex.lock();
+    int count = 0;
+    for (size_t i = left; i < right; i++)
+    {
+        for (size_t j = 0; j < substring.size(); j++)
+        {
+            if (i + j < main_string.size())
+            {
+                count += If_eq(main_string[i + j], substring[j]);
+            }
+        }
+        if (count == substring.size())
+        {
+            Iterators.push_back(i);
+        }
+        count = 0;
+    }
+    _mutex.unlock();
+}
 
-	explicit Threads_Guard(std::vector < std::thread > & threads) :
-		m_threads(threads)
-	{}
-
-	Threads_Guard(Threads_Guard const &) = delete;
-
-	Threads_Guard& operator=(Threads_Guard const &) = delete;
-
-	~Threads_Guard() noexcept
-	{
-		try
-		{
-			for (std::size_t i = 0; i < m_threads.size(); ++i)
-			{
-				if (m_threads[i].joinable())
-				{
-					m_threads[i].join();
-				}
-			}
-		}
-		catch (...)
-		{
-			// std::abort();
-		}
-	}
-
-private:
-
-	std::vector < std::thread > & m_threads;
-};
-
-template < typename Iterator, typename T >
-struct Searcher
+void parallel_search(std::string main_string, std::string substring, std::vector<size_t>& Iterators)
 {
-	void operator()(Iterator first, Iterator last, T element,
-		std::promise < Iterator > & result, std::atomic < bool > & flag) noexcept
-	{
-		try
-		{
-			for (; (first != last) && !flag.load(); ++first)
-			{
-				if (*first == element)
-				{
-					result.set_value(first);
-					flag.store(true);
-					return;
-				}
-			}
-		}
-		catch (...)
-		{
-			try
-			{
-				result.set_exception(std::current_exception());
-				flag.store(true);
-			}
-			catch (...)
-			{
-				// ...
-			}
-		}
-	}
-};
+    const std::size_t num_core = std::thread::hardware_concurrency();
+    int hardware_threads = num_core != 0 ? num_core : 4;
+    int thread_x_size = main_string.size() / hardware_threads;
+    int residue = main_string.size() % hardware_threads;
+    std::vector<std::thread> threads(hardware_threads);
+    double left = 0, right = thread_x_size;
+    for (size_t i = 0; i < threads.size(); i++)
+    {
+        threads[i] = std::thread(Search, main_string, substring, std::ref(Iterators), left, right);
+        left += thread_x_size;
+        right += thread_x_size;
+    }
 
-template < typename Iterator, typename T >
-Iterator parallel_find(Iterator first, Iterator last, T element)
-{
-	const std::size_t length = std::distance(first, last);
-
-	if (!length)
-		return last;
-
-	const std::size_t min_per_thread = 25;
-	const std::size_t max_threads =
-		(length + min_per_thread - 1) / min_per_thread;
-
-	const std::size_t hardware_threads =
-		std::thread::hardware_concurrency();
-
-	const std::size_t num_threads =
-		std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
-
-	const std::size_t block_size = length / num_threads;
-
-	std::promise < Iterator > result;
-	std::atomic < bool > flag(false);
-	std::vector < std::thread > threads(num_threads - 1);
-
-	{
-		Threads_Guard guard(threads);
-
-		Iterator block_start = first;
-
-		for (std::size_t i = 0; i < (num_threads - 1); ++i)
-		{
-			Iterator block_end = block_start;
-			std::advance(block_end, block_size);
-
-			threads[i] = std::thread(Searcher < Iterator, T > (),
-				block_start, block_end, element, std::ref(result), std::ref(flag));
-
-			block_start = block_end;
-		}
-
-		Searcher < Iterator, T > ()(block_start, last, element, result, flag);
-	}
-
-	if (!flag.load())
-	{
-		return last;
-	}
-
-	return result.get_future().get();
+    if (residue != 0)
+    {
+        threads.push_back(std::thread(Search, main_string, substring, std::ref(Iterators), left, main_string.size()));
+    }
+    std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
 }
 
 int main()
 {
+    //генерирую строку
     vector <char> dna = {'A', 'G', 'T', 'C'};
 //    vector <string> dna = {"A", "G", "T", "C"};
 
@@ -156,29 +85,21 @@ int main()
         n = dist(gen);
         st = st + dna[n];
     }
-    cout << st << endl;
+    cout << "generated string: " << st << endl;
 
-    vector <char> f = {'T'};
+    string substring;
+    cout << "Insert the substring you want to find: ";
+    std::cin >> substring;
+    std::vector<size_t> Iterators;
+    parallel_search(st, substring, Iterators);
 
-//    cout << "find: " << f[0] << endl;
-    for (size_t i = 0; i < st.length(); i++) {
-
-        auto result = parallel_find(st.begin() + i, st.end(), f[0]);
-//        auto result = parallel_find(st.begin() + i, st.begin() + i, f[0]);
-        if (result != st.end())
-//        if (result == st.begin() + i)
-            cout << "i: " << i + 1 << endl;
-
-//        result = parallel_find(result + 1, st.end(), f[1]);
-//	auto result = parallel_find(1, 10, "CAT");
+    for (size_t i = 0; i < Iterators.size(); i++) {
+        std::cout << "The beginning of substring: " << Iterators[i] << std::endl;
     }
-//	if (result != st.end())
-//	{
-//		std::cout << "Element found: " << *result << std::endl;
-////		std::cout << "Element found: " << result << std::endl;
-//	}
-//	else {
-//		std::cout << "Element not found." << std::endl;
-//	}
+
+    if (Iterators.size() == 0) {
+        std::cout << "Can't find your substring" << std::endl;
+    }
+
     return 0;
 }
