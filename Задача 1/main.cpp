417623eaa9a049c0b5c8b73050da6370d54bc93a
Ivan-Grudev <vanek-stark@yandex.ru>
2021-04-01 14:31:41 +0300

    добавил lock_guard, проверку на выдавание concurrency нуля (исправив тем самым косяк с того задания), поменял пару обозначений

--- Задача 1/main.cpp
+++ Задача 1/main.cpp
@@ -24,25 +24,28 @@ double pie_seq()
             ++k;
         }
     }
-    return 4 * k / N;
+    return 4 * static_cast<double>(k) / static_cast<double>(N);
 }
 
-void pie(double xm, const int N, double &K)
+void pie(double x_1, double x_2, const int N, double &K)
 {
     double k = 0;
     std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dist(0,1);
-    std::uniform_real_distribution<> dist_2(xm, xm + 0.25);
-    for(auto i = 0; i < N; ++i) {
-        double x = dist_2(gen);
-        double y = dist(gen);
-        if( x*x + y*y <= 1) {
+    std::mt19937 mersenne(rd());
+    std::uniform_real_distribution<> dist(0, 1);
+    std::uniform_real_distribution<> dist_2(x_1, x_2);
+
+    for(auto i = 0; i < N; ++i)
+    {
+        double x = dist_2(mersenne);
+        double y = dist(mersenne);
+        if( x*x + y*y <= 1)
+        {
             ++k;
         }
     }
 
-    g_mutex.lock();
+    std::lock_guard < std::mutex > lock(mutex);
     K += k;
     g_mutex.unlock();
 }
@@ -50,22 +53,22 @@ void pie(double xm, const int N, double &K)
 double pie_par()
 {
     const int N = 100000;
-    int num = std::thread::hardware_concurrency();
-    int n = N / num;
+    int num_core = std::thread::hardware_concurrency();
+    int num_threads = num_core != 0 ? num_core : 4;
+    int n = N / num_threads;
     double k = 0;
+    std::vector < std::thread > th(num_threads);
 
-    std::vector < std::thread > th(num);
-
-    for (std::size_t i = 0; i < num; ++i)
+    for (std::size_t i = 0; i < th.size(); ++i)
 	{
 		th[i] = std::thread(
-			pie, static_cast<double>(i) / static_cast<double>(num),
-            static_cast<double>(i + 1) / static_cast<double>(num),
+			pie, static_cast<double>(i) / static_cast<double>(num_threads),
+            static_cast<double>(i + 1) / static_cast<double>(num_threads),
             n, std::ref(k));
 
 	}
 
-	for(int i = 0; i < num; ++i)
+	for(int i = 0; i < num_threads; ++i)
     {
         th[i].join();
     }
